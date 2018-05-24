2729f7a104831368aee480b6ae97d0ab86014838
Lloyd Lentz <cledwyn@gmail.com>
2018-05-24 10:00:40 -0500

    Initial commit

---
+++ scripts/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBbYJ2uhuXtUcjIU4S_ih3AWbSWi4Ukxzs",
+	authDomain: "lloyd-94466.firebaseapp.com",
+	databaseURL: "https://lloyd-94466.firebaseio.com",
+	projectId: "lloyd-94466",
+	storageBucket: "lloyd-94466.appspot.com",
+	messagingSenderId: "5064550908"
+};
+firebase.initializeApp(config);
